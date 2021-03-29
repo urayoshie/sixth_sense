fac36f3a7aec6580ffb3b689d3aaf5de1f9c39d4
Yoshie <yoshieura@icloud.com>
2021-03-29 13:13:56 +0900

    read meの修正

--- character.rb
+++ character.rb
@@ -2,6 +2,7 @@ class Character
   attr_reader :id, :name, :offense
   attr_accessor :hp
   @@count = 0
+
   def initialize(character_params)
     @id = @@count += 1
     @name = character_params[:name]
